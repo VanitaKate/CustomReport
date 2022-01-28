23ffd7e327b03942400fd55a4788a64eff6424f3
Vanita.Kate <vani_ket@rediffmail.com>
2022-01-28 19:49:07 +0530

    ProjectPush

---
+++ src/test/java/JenkinsParametersTest/VerifyGoogle.java
@@ -0,0 +1,30 @@
+package JenkinsParametersTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class VerifyGoogle {
+public WebDriver driver;
+
+@Test
+public void definebrowser() {
+
+}
+@Test
+	public void OpenGoogle( ) {
+	String Browser=System.setProperty("Browser", "Chrome");
+System.out.println(Browser);
+		if (Browser.contains("Chrome")){
+			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
+			driver=new ChromeDriver();
+		}else if (Browser.contains("Firefox")) {
+			System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"\\Drivers\\geckodriver.exe");
+			driver=new FirefoxDriver();
+			
+		}
+		driver.get("https://google.co.in");
+	}
+
+}
