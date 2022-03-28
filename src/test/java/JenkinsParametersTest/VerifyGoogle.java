66f8b31f879ab5c7ef5de031cd76c17f2bf1fb95
VanitaKate <58419931+VanitaKate@users.noreply.github.com>
2022-03-28 18:47:21 +0530

    update sauce

--- src/test/java/JenkinsParametersTest/VerifyGoogle.java
+++ src/test/java/JenkinsParametersTest/VerifyGoogle.java
@@ -4,13 +4,40 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-
-public class VerifyGoogle {
+import org.testng.annotations.BeforeTest;
+public class VerifyGoogle {   
+	protected RemoteWebDriver Rdriver;
+	public static WebDriverWait wait;
+	public static String Browser;
+	public static String BrowserName;
+	public String User_Name;
+	public String AccessKey;
+	public String hub;
 public WebDriver driver;
 
-@Test
+@BeforeTest
 public void definebrowser() {
+		Browser=System.getProperty("Browser");
+		MutableCapabilities sauceCap=new MutableCapabilities();
+		String User_Name="oauth-vani_ket-3e468";
+		String AccessKey="8e176690-ece8-423e-846d-3910030becb6";
+		sauceCap.setCapability("platform", "Windows 10");
+		sauceCap.setCapability("version", "latest");
+		DesiredCapabilities caps=new DesiredCapabilities();
+		caps.setCapability("sauce:options", sauceCap);	
+		caps.setCapability("browserName", Browser);
+		driver = new RemoteWebDriver(new URL("https://"+User_Name+":"+AccessKey+"@ondemand.eu-central-1.saucelabs.com:443/wd/hub"), caps);	
 
+		System.out.println("Browser Name is :    ***************"+Browser+"**************");
+
+//		sauceOptions.put("name", testInfo.getDisplayName());
+
+//		options.setCapability("sauce:options", sauceOptions);
+
+
+
+		driver.get("Https://google.com");
+		System.out.println(driver.getCurrentUrl());
 }
 @Test
 	public void OpenGoogle( ) {
